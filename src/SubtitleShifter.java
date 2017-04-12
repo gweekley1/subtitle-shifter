8b6c949c7320bfe2080d82899cf3c3fd0680bb54
Grant Weekley <gweekley1@gmail.com>
2017-04-11 23:07:11 -0500

    Remove unused imports, improve error handling, improve comments

--- src/SubtitleShifter.java
+++ src/SubtitleShifter.java
@@ -1,5 +1,4 @@
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -7,7 +6,6 @@ import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.SimpleDateFormat;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,39 +13,51 @@ public class SubtitleShifter {
 	
 	public static final boolean DEBUG = true;
 	
+	/*
+	 * args[0] The path to the srt file being modified
+	 * args[1] The amount of milliseconds by which to shift the subtitles
+	 */
 	public static void main(String[] args) {
 		
-		// open file
+		// extract the subtitles from the file and save them as a string
 		String subtitles = null;
 		try {
 			byte[] fileBytes = Files.readAllBytes(Paths.get(args[0]));
 			subtitles = new String (fileBytes, StandardCharsets.ISO_8859_1);
 		} catch (IOException e) {
 			System.out.println("Failed to find " + args[0]);
+			System.exit(1);
 		}
 		
-		// walk through
+		// walk through the string to finds substrings matching `##:##:##,###`, where # is a number
 		Pattern timePattern = Pattern.compile("[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}");
 		Matcher timeMatch = timePattern.matcher(subtitles);
-
 		while (timeMatch.find()) {
+			
 			String oldTimestamp = timeMatch.group();
+			int index = timeMatch.start();
 			Timestamp newTimestamp = new Timestamp(oldTimestamp);
+			// calculate the new timestamp and replace the value in the subtitles string
 			newTimestamp.shift(Integer.valueOf(args[1]));
-			subtitles = subtitles.replace(oldTimestamp, newTimestamp.toString());
+			subtitles = subtitles.substring(0, index) + newTimestamp + subtitles.substring(index + newTimestamp.toString().length());
 			
 			if (DEBUG)
 				System.out.println("Changed " + oldTimestamp + " to " + newTimestamp.toString());
 		}
+		
+		// write new subtitles to a new file, `newFileName+$oldfileName.srt`
+		String fileName = "new_" + args[0];
 		try {
-			Writer fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("new_" + args[0]), StandardCharsets.ISO_8859_1));
-			fileWriter.write(subtitles);
-			fileWriter.close();
-		} catch (FileNotFoundException e) {
-			
+			Writer fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName), StandardCharsets.ISO_8859_1));
+			try {
+				fileWriter.write(subtitles);
+			} catch (IOException e) {
+				System.out.println("Failed to write to file: " + fileName);
+			} finally {
+				fileWriter.close();
+			}
 		} catch (IOException e) {
-			
+			System.out.println("Failed to open file: " + fileName);
 		}
 	}
-
 }
